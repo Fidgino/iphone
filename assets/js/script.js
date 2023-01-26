2ab81aa4010e284f7151b52ee5fb347af4bf849c
Fidgino <Fidgino@gmail.com>
2023-01-26 17:28:44 +0100

    Uptade Menu

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,14 @@
 // Dom Element
 
 const whiteBar = document.querySelector('#whiteBar');
-const ecranLocked = document.querySelector('.MainIphoneLockedScreen')
+const ecranLocked = document.querySelector('.MainIphone')
 
 // White Bar Animation
 
 whiteBar.addEventListener('click', () => {
     ecranLocked.classList.add('slideIn');
     setTimeout(() => {
+        window.location.href='./views/menu.html'
         ecranLocked.remove()
     }, 1000);
 })
